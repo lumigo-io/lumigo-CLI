adfe051981f7010ecaab1b63ed3c5b1162c09c0d
theburningmonk <theburningmonk@gmail.com>
2019-12-17 17:31:17 +0000

    test: added tests for tail-cloudwatch-logs

--- src/commands/tail-cloudwatch-logs.js
+++ src/commands/tail-cloudwatch-logs.js
@@ -6,34 +6,121 @@ const Promise = require("bluebird");
 const moment = require("moment");
 const inquirer = require("inquirer");
 
-let Logs;
-
 class TailCloudwatchLogsCommand extends Command {
 	async run() {
 		const { flags } = this.parse(TailCloudwatchLogsCommand);
-		const { namePrefix, region, profile, filterPattern } = flags;
+		const { namePrefix, region, profile, filterPattern, interval } = flags;
 
 		global.region = region;
 		global.profile = profile;
 		global.filterPattern = filterPattern;
+		global.interval = interval || 1000;
 
 		checkVersion();
     
 		const AWS = getAWSSDK();
-		Logs = new AWS.CloudWatchLogs();
+		this.Logs = new AWS.CloudWatchLogs();
     
 		this.log(`looking for CloudWatch log groups with prefix [${namePrefix}] in [${region}]`);
-		const logGroupName = await getLogGroupName(namePrefix);
+		const logGroupName = await this.getLogGroupName(namePrefix);
     
-		this.log("looking for log stremas...");
-		const logStreamNames = await getLogStreamNames(logGroupName);
+		this.log(`looking for log streams for [${logGroupName}]...`);
+		const logStreamNames = await this.getLogStreamNames(logGroupName);
     
-		this.log(`polling CLoudWatch log group ${logGroupName}...`);
+		this.log(`polling CLoudWatch log group [${logGroupName}]...`);
 		this.log("press <any key> to stop");
-		await pollLogGroup(logGroupName, logStreamNames);
+		await this.pollLogGroup(logGroupName, logStreamNames);
 
-		process.exit(0);
+		this.exit(0);
 	}
+  
+	async getLogGroupName (namePrefix) {
+		const resp = await this.Logs.describeLogGroups({
+			logGroupNamePrefix: namePrefix
+		}).promise();
+  
+		if (resp.logGroups.length === 1) {
+			this.log("only 1 match found");
+			return resp.logGroups[0].logGroupName;
+		} else if (_.isEmpty(resp.logGroups)) {
+			this.log("no matching log groups, please double check the prefix and region and try again");
+			this.exit(1);
+		} else if (resp.nextToken) {
+			this.log("too many log groups with matching prefix, please provide the full log group name and try again");
+			this.exit(1);
+		} else {
+			const logGroupChoices = resp.logGroups.map(x => x.logGroupName);
+			const { logGroupName } = await inquirer.prompt([
+				{
+					type: "list",
+					name: "logGroupName",
+					message: "Which log group are you looking for?",
+					choices: logGroupChoices
+				}
+			]);
+      
+			return logGroupName;
+		}
+	};
+  
+	async getLogStreamNames (logGroupName) {
+		const resp = await this.Logs.describeLogStreams({
+			logGroupName,
+			descending: true,
+			limit: 50,
+			orderBy: "LastEventTime"
+		}).promise();
+    
+		if (_.isEmpty(resp.logStreams)) {
+			this.log("there are no log streams right now, please try again later");
+			this.exit(1);
+		} else {
+			return resp.logStreams.map(x => x.logStreamName);
+		}
+	};
+  
+	async pollLogGroup (logGroupName, logStreamNames) {
+		let polling = true;
+		const readline = require("readline");
+		readline.emitKeypressEvents(process.stdin);
+		process.stdin.setRawMode(true);
+		const stdin = process.openStdin();
+		stdin.once("keypress", () => {
+			polling = false;
+			console.log("stopping...");
+		});
+    
+		const fetch = async (startTime, endTime, nextToken, acc = []) => {
+			const resp = await this.Logs.filterLogEvents({
+				logGroupName,
+				logStreamNames,
+				interleaved: true,
+				filterPattern: global.filterPattern,
+				startTime,
+				endTime,
+				nextToken
+			}).promise();
+      
+			const logMessages = resp.events.map(x => x.message);
+			if (resp.nextToken) {
+				return fetch(startTime, endTime, resp.nextToken, acc.concat(logMessages));
+			} else {
+				return acc.concat(logMessages);
+			}
+		};
+    
+		// start from 5s ago
+		let startTime = moment.utc(moment.now()).valueOf() - 5000;
+		let endTime = moment.utc(moment.now()).valueOf();
+		while (polling) {
+			const logMessages = await fetch(startTime, endTime);
+			logMessages.forEach(this.log);
+			await Promise.delay(global.interval);
+      
+			startTime = endTime;
+			endTime = moment.utc(moment.now()).valueOf();
+		}
+	};
 }
 
 TailCloudwatchLogsCommand.description = "Tail a CloudWatch Log Group";
@@ -55,100 +142,15 @@ TailCloudwatchLogsCommand.flags = {
 	}),
 	filterPattern: flags.string({
 		char: "f",
-		description: "used to filter the logs",
+		description: "filter pattern for the logs, see https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/FilterAndPatternSyntax.html",
 		required: false
 	}),
-};
-
-const getLogGroupName = async namePrefix => {
-	const resp = await Logs.describeLogGroups({
-		logGroupNamePrefix: namePrefix
-	}).promise();
-
-	if (resp.logGroups.length === 1) {
-		console.log("Only 1 match found.");
-		return resp.logGroups[0].logGroupName;
-	} else if (_.isEmpty(resp.logGroups)) {
-		console.log("no matching log groups, please double check the prefix and region and try again");
-		throw new Error("log groups not found");
-	} else if (resp.nextToken) {
-		console.log("Too many log groups with matching prefix, please provide the full log group name and try again");
-		throw new Error("too many log groups");
-	} else {
-		const logGroupChoices = resp.logGroups.map(x => x.logGroupName);
-		const { logGroupName } = await inquirer.prompt([
-			{
-				type: "list",
-				name: "logGroupName",
-				message: "Which log group are you looking for?",
-				choices: logGroupChoices
-			}
-		]);
-    
-		return logGroupName;
-	}
-};
-
-const getLogStreamNames = async logGroupName => {
-	const resp = await Logs.describeLogStreams({
-		logGroupName,
-		descending: true,
-		limit: 50,
-		orderBy: "LastEventTime"
-	}).promise();
-  
-	if (_.isEmpty(resp.logStreams)) {
-		console.log("there are no log streams right now, please try again later");
-		throw new Error("no log streams");
-	} else {
-		return resp.logStreams.map(x => x.logStreamName);
-	}
-};
-
-const pollLogGroup = async (logGroupName, logStreamNames) => {
-	let polling = true;
-	const readline = require("readline");
-	readline.emitKeypressEvents(process.stdin);
-	process.stdin.setRawMode(true);
-	const stdin = process.openStdin();
-	stdin.once("keypress", () => {
-		polling = false;
-		console.log("stopping...");
-	});
-  
-	const AWS = getAWSSDK();
-	const Logs = new AWS.CloudWatchLogs();
-  
-	const fetch = async (startTime, endTime, nextToken, acc = []) => {
-		const resp = await Logs.filterLogEvents({
-			logGroupName,
-			logStreamNames,
-			interleaved: true,
-			filterPattern: global.filterPattern,
-			startTime,
-			endTime,
-			nextToken
-		}).promise();
-    
-		const logMessages = resp.events.map(x => x.message);
-		if (resp.nextToken) {
-			return fetch(startTime, endTime, resp.nextToken, acc.concat(logMessages));
-		} else {
-			return acc.concat(logMessages);
-		}
-	};
-  
-	// start from 5s ago
-	let startTime = moment.utc(moment.now()).valueOf() - 5000;
-	let endTime = moment.utc(moment.now()).valueOf();
-	while (polling) {
-		const logMessages = await fetch(startTime, endTime);
-		logMessages.forEach(console.log);
-		await Promise.delay(1000);
-    
-		startTime = endTime;
-		endTime = moment.utc(moment.now()).valueOf();
-	}
+	interval: flags.integer({
+		char: "i",
+		description: "interval for polling CloudWatch Logs",
+		required: false,
+		default: 1000
+	})
 };
 
 module.exports = TailCloudwatchLogsCommand;
