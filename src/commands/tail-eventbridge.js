179254ecd426632926a30bea3014c26c382b8614
theburningmonk <theburningmonk@gmail.com>
2019-12-31 19:57:57 +0100

    style: fixed prettier errors

--- src/commands/tail-eventbridge.js
+++ src/commands/tail-eventbridge.js
@@ -7,42 +7,47 @@ require("colors");
 
 class TailEventbridgeCommand extends Command {
 	async run() {
-		const {flags} = this.parse(TailEventbridgeCommand);
+		const { flags } = this.parse(TailEventbridgeCommand);
 		const { ruleName, eventBusName, region, profile } = flags;
-    
+
 		global.region = region;
 		global.profile = profile;
 		global.ruleName = ruleName;
 		global.eventBusName = eventBusName;
 
 		checkVersion();
-    
-		this.log(`finding the rule [${ruleName}] (bus [${eventBusName || "default"}]) in [${region}]`);
+
+		this.log(
+			`finding the rule [${ruleName}] (bus [${eventBusName ||
+				"default"}]) in [${region}]`
+		);
 		const ruleArn = await this.getEventBridgeRule(ruleName, eventBusName);
-   
+
 		await this.pollEventBridge(ruleArn);
 
 		this.exit(0);
 	}
-  
+
 	async getEventBridgeRule(ruleName, eventBusName) {
 		const AWS = getAWSSDK();
 		const EventBridge = new AWS.EventBridge();
-    
+
 		const resp = await EventBridge.describeRule({
 			Name: ruleName,
 			EventBusName: eventBusName
 		}).promise();
-    
+
 		if (resp.State !== "ENABLED") {
-			this.log(`WARNING!
+			this.log(
+				`WARNING!
   The rule [${ruleName}] has been disabled.
-  You won't see events until you enable it.`.yellow.bold);
+  You won't see events until you enable it.`.yellow.bold
+			);
 		}
-    
+
 		return resp.Arn;
 	}
-  
+
 	async createQueue(ruleArn) {
 		const AWS = getAWSSDK();
 		const SQS = new AWS.SQS();
@@ -94,7 +99,7 @@ class TailEventbridgeCommand extends Command {
 			QueueUrl: queueUrl
 		}).promise();
 	}
-  
+
 	async addTarget(queueArn) {
 		const AWS = getAWSSDK();
 		const EventBridge = new AWS.EventBridge();
@@ -104,24 +109,26 @@ class TailEventbridgeCommand extends Command {
 		await EventBridge.putTargets({
 			Rule: global.ruleName,
 			EventBusName: global.eventBusName,
-			Targets: [{
-				Id: targetId,
-				Arn: queueArn,
-			}]
+			Targets: [
+				{
+					Id: targetId,
+					Arn: queueArn
+				}
+			]
 		}).promise();
 	}
-  
+
 	async removeTarget() {
 		const AWS = getAWSSDK();
 		const EventBridge = new AWS.EventBridge();
-    
+
 		await EventBridge.removeTargets({
 			Ids: [global.targetId],
 			Rule: global.ruleName,
-			EventBusName: global.eventBusName            
+			EventBusName: global.eventBusName
 		}).promise();
 	}
-  
+
 	async pollEventBridge(ruleArn) {
 		const { queueUrl, queueArn } = await this.createQueue(ruleArn);
 		await this.addTarget(queueArn);
@@ -167,7 +174,11 @@ class TailEventbridgeCommand extends Command {
 					"Detail-Type": body["detail-type"],
 					Detail: body.detail
 				};
-				this.log(body.time.grey.bold.bgWhite, "\n", JSON.stringify(message, undefined, 2));
+				this.log(
+					body.time.grey.bold.bgWhite,
+					"\n",
+					JSON.stringify(message, undefined, 2)
+				);
 			});
 
 			await SQS.deleteMessageBatch({
