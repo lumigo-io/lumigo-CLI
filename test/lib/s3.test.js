4f86365891adbc02db63a087951eb61ab057e69e
Efi Merdler-Kravitz <efi@lumigo.io>
2020-01-16 07:58:49 +0200

    docs: use only jest

--- test/lib/s3.test.js
+++ test/lib/s3.test.js
@@ -1,10 +1,10 @@
 const { expect } = require("@oclif/test");
-const AWSMock = require("aws-sdk-mock");
 const { getAWSSDK } = require("../../src/lib/aws");
 const { deleteAllBuckets } = require("../../src/lib/s3");
 const chaiAsPromised = require("chai-as-promised");
 const chai = require("chai");
-require("colors"); // Required for avoid fail on console printing
+require("colors");
+const { success, fail } = require("../test-utils/jest-mocks"); // Required for avoid fail on console printing
 
 jest.spyOn(global.console, "log");
 global.console.log.mockImplementation(() => {});
@@ -14,33 +14,46 @@ describe("deleteAllBuckets", () => {
 	let AWS = null;
 	beforeAll(() => {
 		AWS = getAWSSDK();
-		AWSMock.setSDKInstance(AWS);
+	});
+
+	beforeEach(() => {
+		AWS = getAWSSDK();
+		const listBuckets = jest.fn();
+
+		listBuckets.mockImplementation(() => {
+			return {
+				promise() {
+					return Promise.resolve({
+						Buckets: [{ Name: "MyBucket" }]
+					});
+				}
+			};
+		});
+
+		AWS.S3.prototype.listBuckets = listBuckets;
+
+		const listObjectsV2 = jest.fn();
+
+		listObjectsV2.mockImplementation(() => {
+			return {
+				promise() {
+					return Promise.resolve({
+						Contents: [{ Key: "MyItem.txt" }]
+					});
+				}
+			};
+		});
+
+		AWS.S3.prototype.listObjectsV2 = listObjectsV2;
+		AWS.S3.prototype.deleteObjects = success;
 	});
 
 	afterEach(() => {
-		AWSMock.restore();
+		jest.restoreAllMocks();
 	});
 
 	it("Successful delete of a single bucket", async function() {
-		AWSMock.mock("S3", "listBuckets", function(callback) {
-			callback(null, {
-				Buckets: [{ Name: "MyBucket" }]
-			});
-		});
-
-		AWSMock.mock("S3", "listObjectsV2", function(params, callback) {
-			callback(null, {
-				Contents: [{ Key: "MyItem.txt" }]
-			});
-		});
-
-		AWSMock.mock("S3", "deleteObjects", function(params, callback) {
-			callback(null, {});
-		});
-
-		AWSMock.mock("S3", "deleteBucket", function(params, callback) {
-			callback(null, {});
-		});
+		AWS.S3.prototype.deleteBucket = success;
 
 		const result = await deleteAllBuckets(AWS);
 
@@ -50,31 +63,13 @@ describe("deleteAllBuckets", () => {
 	});
 
 	it("Failed delete of a single bucket", async function() {
-		AWSMock.mock("S3", "listBuckets", function(callback) {
-			callback(null, {
-				Buckets: [{ Name: "MyBucket" }]
-			});
-		});
-
-		AWSMock.mock("S3", "listObjectsV2", function(params, callback) {
-			callback(null, {
-				Contents: [{ Key: "MyItem.txt" }]
-			});
-		});
-
-		AWSMock.mock("S3", "deleteObjects", function(params, callback) {
-			callback(null, {});
-		});
-
-		AWSMock.mock("S3", "deleteBucket", function(params, callback) {
-			callback(new Error("Boom!"));
-		});
+		AWS.S3.prototype.deleteBucket = fail;
 
 		const result = await deleteAllBuckets(AWS);
 
 		expect(result.length).to.equal(1);
 		expect(result[0].name).to.equal("MyBucket");
-		expect(result[0].reason.message).to.equal("Boom!");
+		expect(result[0].reason.message).to.equal("");
 		expect(result[0].status).to.equal("fail");
 	});
 });
