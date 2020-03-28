6525a9ec060a65ec575efdcb85b3683c7e774bd4
theburningmonk <theburningmonk@gmail.com>
2020-03-28 04:09:45 +0100

    feat: replay SQS messages to diff account & region
    Closes #40

--- src/lib/aws.js
+++ src/lib/aws.js
@@ -1,13 +1,21 @@
 process.env.AWS_NODEJS_CONNECTION_REUSE_ENABLED = "1";
+const _ = require("lodash");
 
-const getAWSSDK = () => {
+const getAWSSDK = (options) => {
 	const AWS = require("aws-sdk");
 
-	if (global.region) {
+	if (_.get(options, "region")) {
+		AWS.config.region = options.region;
+	} else if (global.region) {
 		AWS.config.region = global.region;
 	}
 
-	if (global.profile) {
+	if (_.get(options, "profile")) {
+		const credentials = new AWS.SharedIniFileCredentials({
+			profile: options.profile
+		});
+		AWS.config.credentials = credentials;
+	} else if (global.profile) {
 		const credentials = new AWS.SharedIniFileCredentials({
 			profile: global.profile
 		});
