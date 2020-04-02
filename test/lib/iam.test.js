e6219876b59a286a3ec774cc5c9ebcf193db996f
Efi Merdler-Kravitz <efi@lumigo.io>
2020-04-02 17:05:26 +0300

    feat: clear all policies

--- test/lib/iam.test.js
+++ test/lib/iam.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("@oclif/test");
 const { getAWSSDK } = require("../../src/lib/aws");
-const { deleteAllRoles } = require("../../src/lib/iam");
+const { deleteAllRoles, deleteAllPolicies } = require("../../src/lib/iam");
 const chaiAsPromised = require("chai-as-promised");
 const chai = require("chai");
 require("colors"); // Required for avoid fail on console printing
@@ -81,6 +81,71 @@ describe("deleteAllRoles", () => {
 		const result = await deleteAllRoles(AWS);
 
 		expect(result.length).to.equal(1);
+		expect(result[0].status).to.equal("fail");
+	});
+});
+
+describe("deleteAllProfiles", () => {
+	let AWS = null;
+	beforeEach(() => {
+		AWS = getAWSSDK();
+
+		AWS.IAM.prototype.listPolicies = getPromiseResponse({
+			Policies: [
+				{
+					Arn: "aws:policy1",
+					PolicyName: "my_policy1"
+				},
+				{ Arn: "aws:policy2", PolicyName: "my_policy2" }
+			]
+		});
+
+		AWS.IAM.prototype.deletePolicy = success;
+	});
+
+	afterEach(() => {
+		jest.restoreAllMocks();
+	});
+
+	it("Successful delete all policies", async function() {
+		AWS.IAM.prototype.deleteRole = success;
+
+		const result = await deleteAllPolicies(AWS);
+
+		expect(result.length).to.equal(2);
+		expect(result[0].status).to.equal("success");
+	});
+
+	it("Failed deleting policy", async function() {
+		AWS.IAM.prototype.deletePolicy = fail;
+
+		const result = await deleteAllPolicies(AWS);
+
+		expect(result.length).to.equal(2);
+		expect(result[0].status).to.equal("fail");
+	});
+
+	it("Failed deleting IAM policy, retry once, then fail", async function() {
+		const fail = jest.fn();
+		let counter = 1;
+		fail.mockImplementation(() => {
+			return {
+				promise() {
+					if (counter === 1) {
+						counter++;
+						return Promise.reject({ code: "Throttling" });
+					} else {
+						return Promise.reject(new Error());
+					}
+				}
+			};
+		});
+
+		AWS.IAM.prototype.deletePolicy = fail;
+
+		const result = await deleteAllPolicies(AWS);
+
+		expect(result.length).to.equal(2);
 		expect(result[0].status).to.equal("fail");
 	});
 });
